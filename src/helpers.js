061ecee17876437a7bde386f79fe5273d3fc3670
lordSosa15 <97013669+lordSosa15@users.noreply.github.com>
2022-06-24 16:08:05 -0400

    stripe3

--- src/helpers.js
+++ src/helpers.js
@@ -1,15 +1,14 @@
-
-const API = process.env.ORIGIN || 'https://magenta-cendol-be6ad4.netlify.app';
+// const API = process.env.ORIGIN || 'https://magenta-cendol-be6ad4.netlify.app';
 
 export async function fetchFromAPI(endpoint, opts) {
-    const { method, body} = {method: 'POST', body: null, ...opts};
+  const { method, body } = { method: "POST", body: null, ...opts };
 
-    const res = await fetch(`${API}/${endpoint}`, {
-        method,
-        ...(body && {body: JSON.stringify(body)}),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    return res.json()
+  const res = await fetch(`/${endpoint}`, {
+    method,
+    ...(body && { body: JSON.stringify(body) }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return res.json();
 }
